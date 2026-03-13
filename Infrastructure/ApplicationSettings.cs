130a7908d7caf1dac1f33f95014f3ecda1265fa3
Richard Wilde <richard@wildesoft.net>
2026-03-13 12:42:29 +0000

    Update .gitignore, configure database settings, and add additional services to compose file

--- Infrastructure/ApplicationSettings.cs
+++ Infrastructure/ApplicationSettings.cs
@@ -2,5 +2,5 @@ namespace Container.Infrastructure;
 
 public static class ApplicationSettings
 {
-    public static string DbConnectionString => "Data Source=127.0.0.1;Initial Catalog=test;UID=user;PWD=password;";
+    public static string DbConnectionString => "Data Source=127.0.0.1,3307;Initial Catalog=test;UID=user;PWD=password;";
 }
